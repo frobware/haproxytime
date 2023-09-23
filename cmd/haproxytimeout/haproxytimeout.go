1630a044deca1d3c6f45f7a5699d0421451b5c65
Andrew McDermott <aim@frobware.com>
2023-09-23 09:28:45 +0100

    cmd/haproxytimeout: add export_test for unit testing
    The goal is to ensure that the unit testing for cmd/haproxytimeout
    only uses public functions and is tested in the same way the utility
    can actually be excercised via interactive use or from a script. The
    export_test.go explicitly and visibly lists the leaked/tested
    functions.

--- cmd/haproxytimeout/haproxytimeout.go
+++ cmd/haproxytimeout/haproxytimeout.go
@@ -271,7 +271,7 @@ func getInputSource(rdr io.Reader, remainingArgs []string) (string, error) {
 	return readAll(rdr)
 }
 
-// ConvertDuration is the primary logic function for the
+// convertDuration is the primary logic function for the
 // haproxytimeout tool. It parses command-line flags, reads input for
 // a duration string (either from arguments or stdin), converts it
 // into a Go time.Duration object, and then outputs the result.
@@ -296,7 +296,7 @@ func getInputSource(rdr io.Reader, remainingArgs []string) (string, error) {
 // If an error occurs, the function writes the error message to stderr
 // and returns 1. Otherwise, it writes the converted or maximum
 // duration to stdout and returns 0.
-func ConvertDuration(stdin io.Reader, stdout, stderr io.Writer, args []string) int {
+func convertDuration(stdin io.Reader, stdout, stderr io.Writer, args []string) int {
 	fs := flag.NewFlagSet("haproxytimeout", flag.ContinueOnError)
 	fs.SetOutput(io.Discard)
 
@@ -348,5 +348,5 @@ func ConvertDuration(stdin io.Reader, stdout, stderr io.Writer, args []string) i
 }
 
 func main() {
-	os.Exit(ConvertDuration(os.Stdin, os.Stdout, os.Stderr, os.Args[1:]))
+	os.Exit(convertDuration(os.Stdin, os.Stdout, os.Stderr, os.Args[1:]))
 }
