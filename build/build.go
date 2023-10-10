d1d50b208fd38c8c60a3d6d14ceb82582961820e
Andrew McDermott <aim@frobware.com>
2023-10-10 21:19:56 +0100

    Update build.go and version.nix for version 0.3.7

--- build/build.go
+++ build/build.go
@@ -3,7 +3,7 @@ package build
 // Automatically generated. DO NOT EDIT.
 
 var (
-	Version   = "0.3.6"
+	Version   = "0.3.7"
 	Date      = "2023-10-10"
 	GoVersion = "go version go1.20.8 linux/amd64"
 )
--- version.nix
+++ version.nix
@@ -1,2 +1,2 @@
 # Automatically generated. DO NOT EDIT.
-{ version = "0.3.6"; date = "2023-10-10"; goversion = "go version go1.20.8 linux/amd64"; }
+{ version = "0.3.7"; date = "2023-10-10"; goversion = "go version go1.20.8 linux/amd64"; }
